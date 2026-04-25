68b47ca517f37017c1093cd5ad3ef8eb8d7ec84e
ylshaw1421 <ylshaw1421@gmail.com>
2026-04-25 12:30:22 -0500

    Update streamlit_app.py
    Pull all values from API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-LUsmoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")  
-st.text(LUsmoothiefroot_response)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -58,4 +56,4 @@ if ingredients_list:
 
 
 LUsmoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")  
-st.text(LUsmoothiefroot_response)
+st.text(LUsmoothiefroot_response.json())
